85ba452179379cb344246aecc46cd6d1bbc78a8d
Bertrand Zuchuat <bertrand.zuchuat@gmail.com>
2011-01-27 21:41:54 +0100

    Changed web controller

--- symfony2project.php
+++ symfony2project.php
@@ -343,7 +343,7 @@ require_once __DIR__.'/../app/AppKernel.php';
 use Symfony\Component\HttpFoundation\Request;
 
 $kernel = new AppKernel('prod', false);
-$kernel->handle(new Request())->send();
+$kernel->handle(Request::createFromGlobals())->send();
 EOF;
 
 $index_dev = <<<'EOF'
@@ -360,7 +360,7 @@ require_once __DIR__.'/../app/AppKernel.php';
 use Symfony\Component\HttpFoundation\Request;
 
 $kernel = new AppKernel('dev', true);
-$kernel->handle(new Request())->send();
+$kernel->handle(Request::createFromGlobals())->send();
 EOF;
 
 
