0ce744f7fce8186d0476f53f79bc50bf992f7193
Bertrand Zuchuat <bertrand.zuchuat@gmail.com>
2011-02-06 17:28:42 +0100

    removed the need to define getNamespace() and getPath() in bundle

--- symfony2project.php
+++ symfony2project.php
@@ -176,21 +176,6 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class %app%Bundle extends Bundle
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function getNamespace()
-    {
-        return __NAMESPACE__;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getPath()
-    {
-        return strtr(__DIR__, '\\', '/');
-    }
 }
 EOF;
 
